9e53a3c1ddcb6bf4f0ec8e7c882ad0dfca0cd0e0
Koshan Qari <koshanqari@hotmail.com>
2021-12-21 21:54:17 +0530

    initial commit of the master branch

---
+++ KQari Cipher.py
@@ -0,0 +1,62 @@
+import os
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',]
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+welcome = '''    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Welcome to KQari's Caesar_Cipher encoder-decoder
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'''
+
+cont = 'yes'
+while cont == 'yes':
+  os.system('cls')
+  
+  print(logo)
+  print(welcome)
+  print('\n')
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))%26
+
+
+
+  def cipher(text1, shift1, direction1):
+
+    en_text = ""
+    for i in text1:
+      if i in alphabet:
+        loc = alphabet.index(i)
+        if direction1 == 'encode':
+          new_loc = loc + shift1
+        else:
+          new_loc = loc - shift1
+        en_text += alphabet[new_loc]
+      else:
+        en_text += i
+    return en_text
+
+  print(f"Here is your {direction}d text :\n{cipher(text, shift, direction)} ")
+  cont = input("\n\nDo you want to continue 'yes' or 'no': ")
+
+commit 1 in dev
+
+commit 2 in the dev 
+
+
+
+
+ 
